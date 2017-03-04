4a6040248968a948b466527fdb1f197b63d1862d
TavisMolyneux <Tavismolyneux@gmail.com>
2017-03-04 18:37:35 +1100

    created a testing scene and mucked around making some simple mechanics

---
+++ Assets/Bullet_script.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet_script : MonoBehaviour 
+{
+	public bool Speed;
+	public bool Damage;
+	public ParticleSystem Blood;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnCollisionEnter(Collision collison)
+	{
+		if (collison.gameObject.tag == "Enemy") 
+		{
+			print ("hit");
+			collison.gameObject.SendMessage ("Damage", 10f);
+			Instantiate (Blood);
+		}
+
+		Destroy (this.gameObject);
+
+	}
+}
